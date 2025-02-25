c2db14d7df6a4c6f1d2723144cf3dddfac95041a
jeessicats <jeessicats@gmail.com>
2025-02-25 14:54:07 -0300

    Develop the Main App

--- 01-jpa-one-to-one-uni/src/main/java/com/jeessicats/cruddemo/CruddemoApplication.java
+++ 01-jpa-one-to-one-uni/src/main/java/com/jeessicats/cruddemo/CruddemoApplication.java
@@ -1,5 +1,8 @@
 package com.jeessicats.cruddemo;
 
+import com.jeessicats.cruddemo.dao.AppDAO;
+import com.jeessicats.cruddemo.entity.Instructor;
+import com.jeessicats.cruddemo.entity.InstructorDetail;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,10 +16,31 @@ public class CruddemoApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner() {
+	public CommandLineRunner commandLineRunner(AppDAO appDAO) {
 		return runner -> {
-			System.out.println("Hello World!");
+			createInstructor(appDAO);
 		};
+	}
+
+	private void createInstructor(AppDAO appDAO) {
+
+		// create the instructor
+		Instructor tempInstructor = new Instructor("Olivia", "Willow", "olivia@example.com");
+
+		// create the instructor detail
+		InstructorDetail tempInstructorDetail = new InstructorDetail("https://www.youtube.com/olivia", "Reading");
+
+		// associate the instructor with the instructor detail
+		tempInstructor.setInstructorDetail(tempInstructorDetail);
+
+		// save the instructor
+
+		// NOTE: this will also save the details object because of CascadeType.ALL
+
+		System.out.println("Saving instructor: " + tempInstructor);
+		appDAO.save(tempInstructor);
+
+		System.out.println("Done!");
 	}
 
 }
